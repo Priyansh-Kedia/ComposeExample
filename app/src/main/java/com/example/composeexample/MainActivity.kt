a8377f407be85c90ebdb023edec0517f2b299e97
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-05-24 03:07:12 +0530

    added jetpack compose 4

--- app/src/main/java/com/example/composeexample/MainActivity.kt
+++ app/src/main/java/com/example/composeexample/MainActivity.kt
@@ -6,15 +6,18 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -28,7 +31,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageList(listOf("1","2"))
+            Column {
+                MessageList(listOf("1","2"))
+                ShowButton()
+            }
         }
     }
 }
@@ -55,9 +61,12 @@ fun MessageList(
 
            Spacer(modifier = Modifier.height(16.dp))
 
-           messages.forEach { message ->
-               MessageRow(message)
-           }
+          Column {
+              messages.forEach { message ->
+                  MessageRow(message)
+                  Divider(color = Color.Black)
+              }
+          }
        }
 
 }
@@ -66,21 +75,53 @@ fun MessageList(
 fun MessageRow(message: String) {
     val text = "Message is $message"
     val context = LocalContext.current
+    var isSelected by remember { mutableStateOf(false) }
     val typography = MaterialTheme.typography
-    Text(
-        text = text,
-        modifier = Modifier
-            .clickable(
-                onClick = {
-                    handleClick(context, message)
-                }
-            ),
-        style = typography.h5,
-        maxLines = 2,
-        overflow = TextOverflow.Ellipsis
-    )
+    val backgroundColor by animateColorAsState(if(isSelected) Color.Red else Color.Yellow)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = text,
+                modifier = Modifier
+                    .clickable(
+                        onClick = {
+                            handleClick(context, text)
+                            isSelected = !isSelected
+                        }
+                    )
+                    .align(Alignment.CenterHorizontally)
+                    .background(color = backgroundColor),
+                style = typography.h5,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
 }
 
+@Composable
+fun ShowButton() {
+    val count = remember { mutableStateOf(0) }
+
+    Button(
+        onClick = {
+        count.value++
+    },
+    colors = ButtonDefaults.buttonColors(
+        backgroundColor = if(count.value % 5 == 0) Color.Green else Color.Yellow
+    ),
+    modifier = Modifier
+        .wrapContentWidth(unbounded = true)
+    ) {
+        Text(text = "I have been clicked ${count.value} times",
+        modifier = Modifier
+            .wrapContentWidth(align = Alignment.CenterHorizontally)
+        )
+    }
+}
 
 private fun handleClick(context: Context, message: String) {
     Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
